0516199ff4ceec212549360c423044fd0e98f120
Leonard Thieu <leonard.thieu@gmail.com>
2017-08-08 14:49:43 -0400

    Migrate files from toofz-necrodancer.

---
+++ toofz.NecroDancer.Leaderboards.Tests/Constants.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace toofz.NecroDancer.Leaderboards.Tests
+{
+    static class Constants
+    {
+        public static readonly Uri FakeUri = new Uri("http://fake.domain.tld/", UriKind.Absolute);
+    }
+}
---
+++ toofz.NecroDancer.Leaderboards/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyCompany("toofz")]
+[assembly: AssemblyCopyright("Copyright © Leonard Thieu 2015")]
+[assembly: ComVisible(false)]
+[assembly: CLSCompliant(false)]
+[assembly: NeutralResourcesLanguage("en-US")]
+[assembly: AssemblyTitle("toofz.NecroDancer.Leaderboards")]
+[assembly: AssemblyProduct("toofz.NecroDancer.Leaderboards")]
+[assembly: AssemblyVersion("1.1.*")]
+
+[assembly: InternalsVisibleTo("toofz.NecroDancer.Leaderboards.Tests")]
