3df54ef1505613d33316ab37a0f3cc8668527814
Leonard Thieu <leonard.thieu@gmail.com>
2017-10-07 19:53:07 -0400

    Add decompression handler.

---
+++ toofz.NecroDancer.Leaderboards/DecompressionHandler.cs
@@ -0,0 +1,31 @@
+﻿using System.IO;
+using System.IO.Compression;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace toofz.NecroDancer.Leaderboards
+{
+    public sealed class DecompressionHandler : DelegatingHandler
+    {
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            var response = await base.SendAsync(request, cancellationToken).ConfigureAwait(false);
+            var content = response.Content;
+
+            if (content.Headers.ContentEncoding.Contains("gzip"))
+            {
+                var inStream = await content.ReadAsStreamAsync().ConfigureAwait(false);
+                using (var gzip = new GZipStream(inStream, CompressionMode.Decompress))
+                {
+                    var outStream = new MemoryStream();
+                    await gzip.CopyToAsync(outStream, 1024, cancellationToken).ConfigureAwait(false);
+                    response.Content = new StreamContent(outStream);
+                }
+                content.Dispose();
+            }
+
+            return response;
+        }
+    }
+}
