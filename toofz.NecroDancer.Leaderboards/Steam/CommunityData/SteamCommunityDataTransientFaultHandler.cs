c0a30943cd129c4ea833362e63c5df4562d80833
Leonard Thieu <leonard.thieu@gmail.com>
2017-11-08 13:03:31 -0500

    Add documentation.

--- toofz.NecroDancer.Leaderboards/Steam/CommunityData/SteamCommunityDataTransientFaultHandler.cs
+++ toofz.NecroDancer.Leaderboards/Steam/CommunityData/SteamCommunityDataTransientFaultHandler.cs
@@ -4,6 +4,9 @@ using Microsoft.Practices.EnterpriseLibrary.TransientFaultHandling;
 
 namespace toofz.NecroDancer.Leaderboards.Steam.CommunityData
 {
+    /// <summary>
+    /// Enables automatic retries for requests to Steam Community Data on transient faults.
+    /// </summary>
     public sealed class SteamCommunityDataTransientFaultHandler : TransientFaultHandlerBase
     {
         private static readonly ILog Log = LogManager.GetLogger(typeof(SteamCommunityDataTransientFaultHandler));
@@ -11,6 +14,9 @@ namespace toofz.NecroDancer.Leaderboards.Steam.CommunityData
         private static readonly RetryStrategy RetryStrategy = new ExponentialBackoff(10, TimeSpan.FromSeconds(1), TimeSpan.FromSeconds(20), TimeSpan.FromSeconds(2));
         private static readonly RetryPolicy RetryPolicy = SteamCommunityDataTransientErrorDetectionStrategy.CreateRetryPolicy(RetryStrategy, Log);
 
+        /// <summary>
+        /// Initializes an instance of the <see cref="SteamCommunityDataTransientFaultHandler"/> class.
+        /// </summary>
         public SteamCommunityDataTransientFaultHandler() : base(RetryPolicy) { }
     }
 }
--- toofz.NecroDancer.Leaderboards/TransientFaultHandlerBase.cs
+++ toofz.NecroDancer.Leaderboards/TransientFaultHandlerBase.cs
@@ -6,8 +6,20 @@ using Microsoft.Practices.EnterpriseLibrary.TransientFaultHandling;
 
 namespace toofz.NecroDancer.Leaderboards
 {
+    /// <summary>
+    /// Provides the base implementation for transient fault handlers.
+    /// </summary>
     public abstract class TransientFaultHandlerBase : DelegatingHandler
     {
+        /// <summary>
+        /// Initializes an instance of the <see cref="TransientFaultHandlerBase"/> class with the specified retry policy.
+        /// </summary>
+        /// <param name="retryPolicy">
+        /// The <see cref="RetryPolicy"/> that executes retries on transient faults.
+        /// </param>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="retryPolicy"/> is null.
+        /// </exception>
         protected TransientFaultHandlerBase(RetryPolicy retryPolicy)
         {
             this.retryPolicy = retryPolicy ?? throw new ArgumentNullException(nameof(retryPolicy));
@@ -15,25 +27,37 @@ namespace toofz.NecroDancer.Leaderboards
 
         private readonly RetryPolicy retryPolicy;
 
+        /// <summary>
+        /// Sends an HTTP request to the inner handler to send to the server as an asynchronous operation.
+        /// </summary>
+        /// <param name="request">The HTTP request message to send to the server.</param>
+        /// <param name="cancellationToken">A cancellation token to cancel operation.</param>
+        /// <returns>
+        /// The task object representing the asynchronous operation.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="request"/> is null.
+        /// </exception>
         protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
         {
-            var retryCount = 0;
+            var isInitialSend = true;
 
             return retryPolicy.ExecuteAsync(async () =>
             {
-                if (retryCount > 0)
+                // When retrying, the request must be cloned as the same request object cannot be sent more than once.
+                if (!isInitialSend)
                 {
                     using (var oldRequest = request)
                     {
                         request = await oldRequest.CloneAsync().ConfigureAwait(false);
                     }
                 }
-                retryCount++;
 
                 var response = await base.SendAsync(request, cancellationToken).ConfigureAwait(false);
-
                 if (!response.IsSuccessStatusCode)
                 {
+                    isInitialSend = false;
+
                     throw new HttpRequestStatusException(response.StatusCode, response.RequestMessage.RequestUri);
                 }
 
