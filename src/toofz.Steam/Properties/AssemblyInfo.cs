c9bb66515502908dd367df3020ddb0fb8bb65193
Leonard Thieu <leonard.thieu@gmail.com>
2017-12-27 16:40:03 -0500

    build: update build system

--- src/toofz.Steam/Properties/AssemblyInfo.cs
+++ src/toofz.Steam/Properties/AssemblyInfo.cs
@@ -1,18 +1,4 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyVersion("0.0.0.0")]
-[assembly: AssemblyFileVersion("0.0.0.0")]
-[assembly: AssemblyInformationalVersion("0.0.0-dev")]
-
-
-[assembly: AssemblyCopyright("Copyright © Leonard Thieu 2017")]
-[assembly: AssemblyProduct("toofz")]
-
-[assembly: AssemblyTitle("toofz Steam")]
-
-[assembly: ComVisible(false)]
+﻿using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("toofz.Steam.Tests")]
 [assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
